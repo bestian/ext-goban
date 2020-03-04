4ca41057e3a23d4870d008d8d21d4785e056cc63
Bestian Tang <bestian@gmail.com>
2020-03-04 12:37:35 +0800

    bug

--- popup.js
+++ popup.js
@@ -1,7 +1,27 @@
-var getTab = function(tab){
-    window.alert(tab.url);
-};
+var postAjax = function(url, data, success) {   
+	window.alert(url)
+	$.ajax({
+	    url: url,
+	    type: 'POST',
+	    contentType: 'text/csv',
+	    processData: false,
+	    data: data
+	}).done(function() {
+    alert( "success" );
+	})
+	.fail(function() {
+	  alert( "error" );
+	})
+	.always(function() {
+	  alert( "complete" );
+	});
+}
 
+var getTab = function(tab){
+	var id = document.getElementById('gobanID').value;
+	var lev = document.getElementById('gobanLev').value;
+    postAjax('https://www.ethercalc.org/_/' + id + lev, tab.url+','+ tab.title, function(data){ window.alert(data); });
+};
 
 
 document.addEventListener('DOMContentLoaded', function() {
